2993ab8413e39acf4508563e4ec4857693f46377
Priyanshu Arora <priyanshu@gkmit.co>
2023-11-22 10:29:57 +0530

    optimizing

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from accounts import views
+from . import views as api_views
 
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
@@ -9,4 +10,7 @@ urlpatterns = [
     path("ticket-types/", include("events_tickets.ticket_type_urls")),
     path("tickets/", include("events_tickets.ticket_urls")),
     path("wishlist/", include("events_tickets.urls")),
+    path("test_create/", api_views.create_sample_tickets),
+    path("test_dict/", api_views.create_notification),
+    path("test_model/", api_views.send_mail_to_attendees),
 ]
